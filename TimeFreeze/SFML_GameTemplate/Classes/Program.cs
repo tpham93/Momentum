4440bbea6020032b662577575a687b6bc5a993a0
Kai Bornemann <kaibornemann@yahoo.de>
2014-01-24 20:02:46 +0100

    - nun sollte es gehen

---
+++ TimeFreeze/SFML_GameTemplate/Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    public static void Main(String[] args)
+    {
+        Console.Write("Test");
+    }
+}
+
