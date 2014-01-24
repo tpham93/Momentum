dbc21612975f7ca9241fb142d6aa7798903a0087
Kai Bornemann <kaibornemann@yahoo.de>
2014-01-24 20:56:04 +0100

    asd

--- TimeFreeze/SFML_GameTemplate/Classes/IGameState.cs
+++ TimeFreeze/SFML_GameTemplate/Classes/IGameState.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-enum EGameState {None, MainMenu, InGame, };
+enum EGameState {None, MainMenu, InGame, EGameStateCount };
 
 interface IGameState
 {
