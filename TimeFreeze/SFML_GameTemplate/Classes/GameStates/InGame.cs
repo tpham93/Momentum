04e3bd5bcb02c31063cedff4994cf12d7f3e0d77
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-01-25 11:38:45 +0100

    new floor

--- TimeFreeze/SFML_GameTemplate/Classes/GameStates/InGame.cs
+++ TimeFreeze/SFML_GameTemplate/Classes/GameStates/InGame.cs
@@ -13,7 +13,8 @@ class InGame : IGameState
     List<Object> worldObjects;
 
     private Level level;
-    Sprite floor;
+    Sprite[] floor;
+
 
     Texture menubarTexture;
     Sprite menubarSprite;
@@ -26,11 +27,23 @@ class InGame : IGameState
 
     public static LevelID levelId;
 
-    
+    private int[,] floorMap;
+    Random random;
 
     public InGame()
     {
-        
+        random = new Random();
+        floorMap = new int[Constants.WINDOWWIDTH / 16,Constants.WINDOWHEIGHT/ 16];
+        for (int x = 0; x < Constants.WINDOWWIDTH / 16; x++)
+        {
+            for (int y = 0; y < Constants.WINDOWHEIGHT / 16; y++)
+            {
+                floorMap[x, y] = random.Next(3);
+
+
+            }
+
+        }
     }
 
     public void Initialize()
@@ -41,8 +54,10 @@ class InGame : IGameState
 
         
 
-        
-        floor = new Sprite(Objects.objektTextures[0], new IntRect(0, 0, 16, 16));
+        floor = new Sprite[3];
+        floor[0] = new Sprite(Objects.objektTextures[0], new IntRect(0, 0, 16, 16));
+        floor[1]= new Sprite(Objects.objektTextures[4], new IntRect(0, 0, 16, 16));
+        floor[2] = new Sprite(Objects.objektTextures[5], new IntRect(0, 0, 16, 16));
 
         //Menubar ini
         menubarSprite = new Sprite(menubarTexture, new IntRect(0, 0, 230, 48));
@@ -104,8 +119,21 @@ class InGame : IGameState
         {
             for (uint y = 0; y < Constants.WINDOWHEIGHT / 16; y++)
             {
-                floor.Position = new SFML.Window.Vector2f(x * 16, y * 16);
-                window.Draw(floor);
+                if (floorMap[x, y] == 0)
+                {
+                    floor[0].Position = new SFML.Window.Vector2f(x * 16, y * 16);
+                    window.Draw(floor[0]);
+                }
+                else if (floorMap[x, y] == 1)
+                {
+                    floor[1].Position = new SFML.Window.Vector2f(x * 16, y * 16);
+                    window.Draw(floor[1]);
+                }
+                else
+                {
+                    floor[2].Position = new SFML.Window.Vector2f(x * 16, y * 16);
+                    window.Draw(floor[2]);
+                }
             }
         }
 
