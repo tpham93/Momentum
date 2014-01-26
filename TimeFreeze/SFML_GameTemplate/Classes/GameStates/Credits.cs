57a4c49e6db3733916d9e4e7e4bf1816020438b5
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-01-26 15:17:35 +0100

    dfhkjljhgfdshrtfdgfdfgds

--- TimeFreeze/SFML_GameTemplate/Classes/GameStates/Credits.cs
+++ TimeFreeze/SFML_GameTemplate/Classes/GameStates/Credits.cs
@@ -10,7 +10,7 @@ using SFML.Graphics;
 class Credits : IGameState
 {
 
-    Text credits = new Text("Visual effects and basic idear: \n   Kai  \nGame structure, Tutorial: \n   Gerd  \nCollision and interaction (Gameplay): \n   Tuan  \nTextures and a little bit of everything: \n   Jarek  \nTextures and Leveldesign: \n   Tobias ", Assets.font);
+    Text credits = new Text("Visual effects and basic idea: \n   Kai  \nGame structure, Tutorial: \n   Gerd  \nCollision and interaction (Gameplay): \n   Tuan  \nTextures and a little bit of everything: \n   Jarek  \nTextures and Leveldesign: \n   Tobias ", Assets.font);
     Texture acagamics = new Texture("Content/logo.png");
     Sprite logo;
 
@@ -22,24 +22,24 @@ class Credits : IGameState
     Text cr = new Text("Credits", Assets.font);
     Text srcHeader = new Text("Sources", Assets.font);
     Text aca = new Text("Acagamics e.V.", Assets.font);
-    Text sources = new Text("http://www.freesound.org/people/fins/sounds/171670/ - success\nLicensed under Creative Commons: By Attribution 0\nhttp://creativecommons.org/publicdomain/zero/1.0/\n------- \nPeace of mind Kevin MacLeod (incompetech.com)\nLicensed under Creative Commons: By Attribution 3.0\nhttp://creativecommons.org/licenses/by/3.0/\n-------\nhttp://www.bfxr.net/ - other sounds\n-------\nhttp://www.1001freefonts.com/primary_elector.font -font", Assets.font);
+    Text sources = new Text("http://www.freesound.org/people/fins/sounds/171670/ - success\nLicensed under Creative Commons: By Attribution 0\nhttp://creativecommons.org/publicdomain/zero/1.0/\n------- \nPeace of mind Kevin MacLeod (incompetech.com)\nLicensed under Creative Commons: By Attribution 3.0\nhttp://creativecommons.org/licenses/by/3.0/\n-------\nhttp://www.freesound.org/people/shanneton/sounds/108600/\nLicensed under Creative Commons: By Attribution-NonCommercial 3.0 Unported\nhttp://creativecommons.org/licenses/by-nc/3.0/\n-------\nhttp://www.bfxr.net/ - other sounds\n-------\nhttp://www.1001freefonts.com/primary_elector.font -font", Assets.font);
 
     public void Initialize()
     {
         cr.Color = Game.AcaOrange;
-        cr.Position = new Vector2f(10, 15);
+        cr.Position = new Vector2f(10, 5);
         cr.Scale = new Vector2f(1, 1);
         aca.Color = Game.AcaOrange;
         aca.Position = new Vector2f(510, 470);
         aca.Scale = new Vector2f(1, 1);
         srcHeader.Color = Color.Green;
-        srcHeader.Position = new Vector2f(10, 350);
+        srcHeader.Position = new Vector2f(10, 300);
         srcHeader.Scale = new Vector2f(1, 1);
         credits.Color = Game.AcaOrange;
-        credits.Position = new Vector2f(10, 60);
+        credits.Position = new Vector2f(10, 40);
         credits.Scale = new Vector2f(0.75f, 0.75f);
         sources.Color = Color.Green;
-        sources.Position = new Vector2f(10, 390);
+        sources.Position = new Vector2f(10, 335);
         sources.Scale = new Vector2f(0.5f, 0.5f);
         logo = new Sprite(acagamics);
         logo.Position = new Vector2f(400, 70);
